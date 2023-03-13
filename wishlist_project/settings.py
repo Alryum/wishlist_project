5e9df29875aefae962d3729d9bef8c4409f9e33e
Max <pixelcat2211@gmail.com>
2023-03-13 16:40:12 +0500

    added env var

--- wishlist_project/settings.py
+++ wishlist_project/settings.py
@@ -1,11 +1,15 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv('dev.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
