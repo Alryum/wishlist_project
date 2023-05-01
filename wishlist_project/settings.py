085c1ca3fdb3b7b39d87fa294a559f1425aeaa1e
Max <pixelcat2211@gmail.com>
2023-05-01 14:31:49 +0500

    /

--- wishlist_project/settings.py
+++ wishlist_project/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #---
+    # ---
     'wishlist_app.apps.WishlistAppConfig',
     'users.apps.UsersConfig',
     'debug_toolbar',
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'wishlist_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
